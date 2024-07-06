7004a30d265de992633e6661a813b723a2ffc944
Elijah Gummer <elijahgummer5@gmail.com>
2024-07-06 21:07:44 +1000

    added some new code

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,9 @@ import SparklesText from "./components/magicui/sparkles-text";
 import AnimatedShinyText from "./components/magicui/animated-shiny-text";
 import BlurIn from "./components/magicui/blur-in";
 import WordPullUp from "./components/magicui/word-pullup";
+import { LampContainer } from "./components/ui/lamp";
+import { motion } from "framer-motion";
+
 
 const people = [
   {
@@ -38,6 +41,18 @@ export type IconProps = React.HTMLAttributes<SVGElement>;
 export function DockDemo() {
   return (
     <div className="footerContainer">
+      <LampContainer>
+      <motion.h1
+        initial={{ opacity: 0.5, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        }}
+        className="mt-8 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
+      ></motion.h1>
+      </LampContainer>
       <footer>
         <div className="social">
           <Dock>
@@ -309,9 +324,9 @@ export default function Home() {
             <BackgroundGradientAnimationDemo />
             <AnimatedTooltipPreview />
             <NavbarDemo />
-            {/* <HeroScrollDemo />
+            <HeroScrollDemo />
             <ThreeDCardDemo />
-            <DockDemo /> */}
+            <DockDemo />
           </div>
         </div>
       </div>
