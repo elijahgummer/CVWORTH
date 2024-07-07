f2d056f0ddf4a17b6383ce32e2ad06aaffbce12c
Elijah Gummer <elijahgummer5@gmail.com>
2024-07-07 18:27:04 +1000

    updating the tailwind.css code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -83,16 +83,16 @@ const config: Config = {
             "background-position": "calc(100% + var(--shimmer-width)) 0",
           },
         },
-        spotlight: {
-          "0%": {
-            opacity: 0,
-            transform: "translate(-72%, -62%) scale(0.5)",
-          },
-          "100%": {
-            opacity: 1,
-            transform: "translate(-50%,-40%) scale(1)",
-          },
       },
+      spotlight: {
+        "0%": {
+          opacity: 0,
+          transform: "translate(-72%, -62%) scale(0.5)",
+        },
+        "100%": {
+          opacity: 1,
+          transform: "translate(-50%,-40%) scale(1)",
+        },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
