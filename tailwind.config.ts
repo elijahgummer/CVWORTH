a55ca1a6ee8f294e01c023f5ad8c88abc18d1667
Elijah Gummer <elijahgummer5@gmail.com>
2024-07-07 18:28:37 +1000

    updating the tailwind.css code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -34,6 +34,17 @@ const config: Config = {
         spotlight: "spotlight 2s ease .75s 1 forwards",
       },
       keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
         moveHorizontal: {
           "0%": {
             transform: "translateX(-50%) translateY(-10%)",
@@ -84,15 +95,6 @@ const config: Config = {
           },
         },
       },
-      spotlight: {
-        "0%": {
-          opacity: 0,
-          transform: "translate(-72%, -62%) scale(0.5)",
-        },
-        "100%": {
-          opacity: 1,
-          transform: "translate(-50%,-40%) scale(1)",
-        },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
