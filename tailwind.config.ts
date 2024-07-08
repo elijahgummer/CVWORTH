39184e6eb8c2cea923058fbd1c88723c51b7cfa7
Elijah Gummer <elijahgummer5@gmail.com>
2024-07-08 12:27:45 +1000

    changed the color of the header

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -85,6 +85,7 @@ const config: Config = {
         },
       },
       colors: {
+        headerColor : '#f2f2f2',
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
