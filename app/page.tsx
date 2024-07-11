985a72d56c20276c7d0de486d942f2b3ea0d96d6
Elijah Gummer <elijahgummer5@gmail.com>
2024-07-11 17:46:27 +1000

    changed the code

--- app/page.tsx
+++ app/page.tsx
@@ -337,7 +337,7 @@ function Navbar({ className }: { className?: string }) {
 
   return (
     <div
-      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+      className={cn("fixed top-5 inset-x-0 w-100% mx-auto z-50", className)}
     >
       <Menu setActive={setActive}>
         <div className="flex items-center justify-start">
