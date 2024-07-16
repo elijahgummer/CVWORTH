17d882de74869f41468b8cfb701a67d4c87634ed
Elijah Gummer <elijahgummer5@gmail.com>
2024-07-16 19:05:53 +1000

    changed the hieght of the hover spring area

--- app/page.tsx
+++ app/page.tsx
@@ -62,7 +62,7 @@ const ProjectsData = [
 const HoverSpring = () => {
   return (
     <div className="flex flex-col overflow-hidden w-full">
-      <div className="h-[40rem] w-full dark:bg-black bg-[#0A0A0A] dark:bg-grid-white/[0.2] bg-grid-black/[0.2] bg-[length:32px_32px] relative flex items-center justify-center">
+      <div className="h-[35rem] w-full dark:bg-black bg-[#0A0A0A] dark:bg-grid-white/[0.2] bg-grid-black/[0.2] bg-[length:32px_32px] relative flex items-center justify-center">
       <div className="flex items-center justify-center w-full flex-col">
         <h1 className="text-headerColor font-bold lg:text-5xl">Discover Our Features</h1>
         <h1 className="text-subHeaderColor font-bold lg:text-2xl mt-5">Explore how we enhance your resume</h1>
@@ -243,14 +243,14 @@ export function ThreeDCardDemo() {
 export function HeroScrollDemo() {
   return (
     <div className="flex flex-col overflow-hidden w-full">
-      <div className="h-[50rem] w-full dark:bg-black bg-[#0A0A0A] dark:bg-grid-white/[0.2] bg-grid-black/[0.2] bg-[length:32px_32px] relative flex items-center justify-center">
+      <div className="h-[60rem] w-full dark:bg-black bg-[#0A0A0A] dark:bg-grid-white/[0.2] bg-grid-black/[0.2] bg-[length:32px_32px] relative flex items-center justify-center">
         <ContainerScroll
           titleComponent={
             <div className="flex items-center justify-center flex-col">
               <div className="z-10 flex items-center justify-center">
                 <div
                   className={cn(
-                    "group rounded-full border m-10 border-black/5 bg-neutral-300 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800"
+                    "group rounded-full border mb-10 border-black/5 bg-neutral-300 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800"
                   )}
                 >
                   <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-600 hover:duration-300 hover:dark:text-neutral-400">
