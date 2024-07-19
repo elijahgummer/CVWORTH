cb37527325e02e5fadf473accbdcbd28b6773ac2
Elijah Gummer <elijahgummer5@gmail.com>
2024-07-19 20:20:22 +1000

    changed the color

--- app/components/ui/navbar-menu.tsx
+++ app/components/ui/navbar-menu.tsx
@@ -43,7 +43,7 @@ export const MenuItem = ({
               <motion.div
                 transition={transition}
                 layoutId="active" // layoutId ensures smooth animation
-                className="bg-black overflow-hidden border border-black/[0.2] dark:border-white/[0.2] shadow-xl"
+                className="overflow-hidden border border-black/[0.2] dark:border-white/[0.2] shadow-xl"
               >
                 <motion.div
                   layout // layout ensures smooth animation
