12192b91eba557af305768f72142092845d282a9
Elijah Gummer <elijahgummer5@gmail.com>
2024-07-27 17:46:19 +1000

    changed the code

--- app/page.tsx
+++ app/page.tsx
@@ -393,7 +393,12 @@ const Navbar = ({ className }: { className?: string }) => {
           {!userId && (
             <>
               <button className="group/button relative inline-flex items-center justify-center overflow-hidden rounded-md bg-gray-800/30 backdrop-blur-lg px-6 py-2 text-base font-semibold text-white transition-all duration-300 ease-in-out hover:scale-110 hover:shadow-xl hover:shadow-gray-600/50 border border-white/20">
-                <span className="text-lg">Sign In</span>
+                <Link
+                  href="sign-in"
+                  className="text-lg text-gray-300 hover:text-white mr-4"
+                >
+                  Sign In
+                </Link>
                 <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-13deg)_translateX(-100%)] group-hover/button:duration-1000 group-hover/button:[transform:skew(-13deg)_translateX(100%)]">
                   <div className="relative h-full w-10 bg-white/20"></div>
                 </div>
