6e7e8b1c139d3dfc2f84935dc93b236129e2b024
Elijah Gummer <elijahgummer5@gmail.com>
2024-07-27 17:44:04 +1000

    inserted a sign in button

--- app/page.tsx
+++ app/page.tsx
@@ -392,14 +392,13 @@ const Navbar = ({ className }: { className?: string }) => {
           <StarButton />
           {!userId && (
             <>
-              <MenuItem setActive={setActive} active={active} item="Sign In">
-                <Link
-                  href="sign-in"
-                  className="text-gray-300 hover:text-white mr-4"
-                >
-                  Sign In
-                </Link>
-              </MenuItem>
+              <button className="group/button relative inline-flex items-center justify-center overflow-hidden rounded-md bg-gray-800/30 backdrop-blur-lg px-6 py-2 text-base font-semibold text-white transition-all duration-300 ease-in-out hover:scale-110 hover:shadow-xl hover:shadow-gray-600/50 border border-white/20">
+                <span className="text-lg">Shimmer</span>
+                <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-13deg)_translateX(-100%)] group-hover/button:duration-1000 group-hover/button:[transform:skew(-13deg)_translateX(100%)]">
+                  <div className="relative h-full w-10 bg-white/20"></div>
+                </div>
+              </button>
+
               <MenuItem setActive={setActive} active={active} item="Sign Up">
                 <Link
                   href="sign-up"
