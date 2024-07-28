147ac22d2b9c26a7db3c8a799b436df5e6e07f0e
Elijah Gummer <elijahgummer5@gmail.com>
2024-07-28 10:52:01 +1000

    changed the code for the sign in page.jsx to center the modal

--- app/sign-in/[[...sign-in]]/page.jsx
+++ app/sign-in/[[...sign-in]]/page.jsx
@@ -2,9 +2,11 @@ import { SignIn } from '@clerk/nextjs';
 
 const SignInPage = () => {
   return (
+    <div className='flex justify-center items-center'>
     <>
       <SignIn />
     </>
+    </div>
   );
 };
 export default SignInPage;
