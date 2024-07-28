0aa9af82d5841b95ed423844e9414e76b56ac54e
Elijah Gummer <elijahgummer5@gmail.com>
2024-07-28 15:20:18 +1000

    deleted the sign in button

--- app/components/ui/Navbar.tsx
+++ app/components/ui/Navbar.tsx
@@ -70,7 +70,6 @@ export function NavbarDemo() {
             <StarButton />
             {!userId && (
               <>
-                <button className="group/button relative inline-flex items-center justify-center overflow-hidden rounded-md bg-gray-800/30 backdrop-blur-lg px-6 py-2 text-base font-semibold text-white transition-all duration-300 ease-in-out hover:scale-110 hover:shadow-xl hover:shadow-gray-600/50 border border-white/20">
                   <Link
                     href="../sign-in/page"
                     className="text-lg text-gray-300 hover:text-white mr-4"
@@ -80,7 +79,7 @@ export function NavbarDemo() {
                   <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-13deg)_translateX(-100%)] group-hover/button:duration-1000 group-hover/button:[transform:skew(-13deg)_translateX(100%)]">
                     <div className="relative h-full w-10 bg-white/20"></div>
                   </div>
-                </button>
+                
   
                 <MenuItem setActive={setActive} active={active} item="Sign Up">
                   <Link
