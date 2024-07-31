7088ce8f8fed925b67b6fbb1b5d6e31a9f98836c
Elijah Gummer <elijahgummer5@gmail.com>
2024-07-31 20:51:20 +1000

    deleted some whitespace

--- app/components/ui/Navbar.tsx
+++ app/components/ui/Navbar.tsx
@@ -50,9 +50,7 @@ const Navbar = ({ className }: { className?: string }) => {
             <div className="mr-5">
                 <Link className="cursor-pointer text-14px text-navbarTabs hover:text-white dark:text-white text-center justify-start items-center" href="/upload">Upload</Link>
             </div>
-            
               <Link href="/policy">Policy</Link>
-            
           </div>
         </div>
         <div className="flex items-center">
