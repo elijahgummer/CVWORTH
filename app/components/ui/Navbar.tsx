d5141aa9cfc8843890d6f68cb308080b723dcbac
Elijah Gummer <elijahgummer5@gmail.com>
2024-07-31 20:49:45 +1000

    added the styling to the link for he Processes tab

--- app/components/ui/Navbar.tsx
+++ app/components/ui/Navbar.tsx
@@ -45,7 +45,7 @@ const Navbar = ({ className }: { className?: string }) => {
                 <Link className="cursor-pointer text-14px text-navbarTabs hover:text-white dark:text-white text-center justify-start items-center" href="/">Home</Link>
             </div>
             <div className="mr-5">
-                <Link href="/process">Process</Link>
+                <Link className="cursor-pointer text-14px text-navbarTabs hover:text-white dark:text-white text-center justify-start items-center" href="/process">Process</Link>
             </div>
             <div className="mr-5">
               <MenuItem setActive={setActive} active={active} item="Upload">
