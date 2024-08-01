804a6bed196527549d3504e50f04e1fdf4cae304
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-01 20:31:00 +1000

    changed the upload href

--- app/components/ui/Navbar.tsx
+++ app/components/ui/Navbar.tsx
@@ -48,7 +48,7 @@ const Navbar = ({ className }: { className?: string }) => {
                 <Link className="cursor-pointer text-14px text-navbarTabs hover:text-white dark:text-white text-center justify-start items-center" href="#uniqueProcess">Process</Link>
             </div>
             <div className="mr-5">
-                {/* <Link className="cursor-pointer text-14px text-navbarTabs hover:text-white dark:text-white text-center justify-start items-center" href="#upload">Upload</Link> */}
+                <Link className="cursor-pointer text-14px text-navbarTabs hover:text-white dark:text-white text-center justify-start items-center" href="#uniqueUpload">Upload</Link>
             </div>
               {/* <Link className="cursor-pointer text-14px text-navbarTabs hover:text-white dark:text-white text-center justify-start items-center" href="#policy">Policy</Link> */}
           </div>
