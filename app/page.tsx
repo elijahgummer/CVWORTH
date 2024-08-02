e87be3cda3bfffb8f73d673816ba78e4a87126b9
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-02 18:30:41 +1000

    deleted the plane icon

--- app/page.tsx
+++ app/page.tsx
@@ -109,25 +109,7 @@ export function AnimatedModalDemo() {
   );
 }
  
-const PlaneIcon = ({ className }: { className?: string }) => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={className}
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M16 10h4a2 2 0 0 1 0 4h-4l-4 7h-3l2 -7h-4l-2 2h-3l2 -4l-2 -4h3l2 2h4l-2 -7h3z" />
-    </svg>
-  );
-};
+
  
 const VacationIcon = ({ className }: { className?: string }) => {
   return (
