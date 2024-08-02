25421d2c43b8f8fe72e09e10b19f14cd5bb47071
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-02 18:46:31 +1000

    changed the component for the dock

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ import { AnimatedTooltip } from "./components/ui/animated-tooltip";
 import { ContainerScroll } from "./components/ui/container-scroll-animation";
 import { CardBody, CardContainer, CardItem } from "./components/ui/3d-card";
 import { SparklesCore } from "./components/ui/sparkles";
-import { Dock, DockIcon } from "./components/magicui/dock";
 import SparklesText from "./components/magicui/sparkles-text";
 import AnimatedShinyText from "./components/magicui/animated-shiny-text";
 import BlurIn from "./components/magicui/blur-in";
@@ -19,6 +18,7 @@ import WordPullUp from "./components/magicui/word-pullup";
 import AnimatedGradientText from "./components/magicui/animated-gradient-text";
 import { motion } from "framer-motion";
 import { NavbarDemo } from "./components/Navbar"; // adjust the path as needed
+import { DockDemo } from "./components/Dock"; // adjust the path as needed
 import {
   Modal,
   ModalBody,
@@ -161,92 +161,7 @@ const HoverSpring = () => {
     </div>
   );
 };
-export type IconProps = React.HTMLAttributes<SVGElement>;
-export function DockDemo() {
-  return (
-    <div className="footerContainer">
-      <footer>
-        <div className="social">
-          <Dock>
-            <DockIcon href="https://github.com/elijahgummer">
-              <Icons.gitHub className="h-6 w-6" />
-            </DockIcon>
-            <DockIcon href="https://twitter.com/codedesigneli">
-              <Icons.twitter className="h-6 w-6" />
-            </DockIcon>
-            <DockIcon href="https://www.linkedin.com/in/elijah-gummer-4168172a9/">
-              <Icons.linkeden className="h-6 w-6" />
-            </DockIcon>
-          </Dock>
-        </div>
-        <ul className="list-inline">
-          <li className="list-inline-item">
-            <a href="#">Home</a>
-          </li>
-          <li className="list-inline-item">
-            <a href="#">Process</a>
-          </li>
-          <li className="list-inline-item">
-            <a href="#">Upload</a>
-          </li>
-          <li className="list-inline-item">
-            <a href="#">Terms</a>
-          </li>
-          <li className="list-inline-item">
-            <a href="#">Policy</a>
-          </li>
-        </ul>
-        <p className={styles.footer}>© 2024 All rights reserved. - CVWORTH</p>
-      </footer>
-    </div>
-  );
-}
-const Icons = {
-  gitHub: (props: IconProps) => (
-    <svg viewBox="0 0 438.549 438.549" {...props}>
-      <path
-        fill="currentColor"
-        d="M409.132 114.573c-19.608-33.596-46.205-60.194-79.798-79.8-33.598-19.607-70.277-29.408-110.063-29.408-39.781 0-76.472 9.804-110.063 29.408-33.596 19.605-60.192 46.204-79.8 79.8C9.803 148.168 0 184.854 0 224.63c0 47.78 13.94 90.745 41.827 128.906 27.884 38.164 63.906 64.572 108.063 79.227 5.14.954 8.945.283 11.419-1.996 2.475-2.282 3.711-5.14 3.711-8.562 0-.571-.049-5.708-.144-15.417a2549.81 2549.81 0 01-.144-25.406l-6.567 1.136c-4.187.767-9.469 1.092-15.846 1-6.374-.089-12.991-.757-19.842-1.999-6.854-1.231-13.229-4.086-19.13-8.559-5.898-4.473-10.085-10.328-12.56-17.556l-2.855-6.57c-1.903-4.374-4.899-9.233-8.992-14.559-4.093-5.331-8.232-8.945-12.419-10.848l-1.999-1.431c-1.332-.951-2.568-2.098-3.711-3.429-1.142-1.331-1.997-2.663-2.568-3.997-.572-1.335-.098-2.43 1.427-3.289 1.525-.859 4.281-1.276 8.28-1.276l5.708.853c3.807.763 8.516 3.042 14.133 6.851 5.614 3.806 10.229 8.754 13.846 14.842 4.38 7.806 9.657 13.754 15.846 17.847 6.184 4.093 12.419 6.136 18.699 6.136 6.28 0 11.704-.476 16.274-1.423 4.565-.952 8.848-2.383 12.847-4.285 1.713-12.758 6.377-22.559 13.988-29.41-10.848-1.14-20.601-2.857-29.264-5.14-8.658-2.286-17.605-5.996-26.835-11.14-9.235-5.137-16.896-11.516-22.985-19.126-6.09-7.614-11.088-17.61-14.987-29.979-3.901-12.374-5.852-26.648-5.852-42.826 0-23.035 7.52-42.637 22.557-58.817-7.044-17.318-6.379-36.732 1.997-58.24 5.52-1.715 13.706-.428 24.554 3.853 10.85 4.283 18.794 7.952 23.84 10.994 5.046 3.041 9.089 5.618 12.135 7.708 17.705-4.947 35.976-7.421 54.818-7.421s37.117 2.474 54.823 7.421l10.849-6.849c7.419-4.57 16.18-8.758 26.262-12.565 10.088-3.805 17.802-4.853 23.134-3.138 8.562 21.509 9.325 40.922 2.279 58.24 15.036 16.18 22.559 35.787 22.559 58.817 0 16.178-1.958 30.497-5.853 42.966-3.9 12.471-8.941 22.457-15.125 29.979-6.191 7.521-13.901 13.85-23.131 18.986-9.232 5.14-18.182 8.85-26.84 11.136-8.662 2.286-18.415 4.004-29.263 5.146 9.894 8.562 14.842 22.077 14.842 40.539v60.237c0 3.422 1.19 6.279 3.572 8.562 2.379 2.279 6.136 2.95 11.276 1.995 44.163-14.653 80.185-41.062 108.068-79.226 27.88-38.161 41.825-81.126 41.825-128.906-.01-39.771-9.818-76.454-29.414-110.049z"
-      ></path>
-    </svg>
-  ),
-  twitter: (props: IconProps) => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      enable-background="new 0 0 72 72"
-      width="22"
-      height="22"
-      viewBox="0 0 72 72"
-      id="twitterx"
-    >
-      <switch>
-        <g fill="#000000" className="color000000 svgShape">
-          <path
-            d="M42.5,31.2L66,6h-6L39.8,27.6L24,6H4l24.6,33.6L4,66
-			h6l21.3-22.8L48,66h20L42.5,31.2z M12.9,10h8l38.1,52h-8L12.9,10z"
-            fill="#ffffff"
-            className="color000000 svgShape"
-          ></path>
-        </g>
-      </switch>
-    </svg>
-  ),
-  linkeden: (props: IconProps) => (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="22"
-      height="22"
-      viewBox="0 5 1036 990"
-      id="Linkedin"
-    >
-      <path
-        d="M0 120c0-33.334 11.667-60.834 35-82.5C58.333 15.833 88.667 5 126 5c36.667 0 66.333 10.666 89 32 23.333 22 35 50.666 35 86 0 32-11.333 58.666-34 80-23.333 22-54 33-92 33h-1c-36.667 0-66.333-11-89-33S0 153.333 0 120zm13 875V327h222v668H13zm345 0h222V622c0-23.334 2.667-41.334 8-54 9.333-22.667 23.5-41.834 42.5-57.5 19-15.667 42.833-23.5 71.5-23.5 74.667 0 112 50.333 112 151v357h222V612c0-98.667-23.333-173.5-70-224.5S857.667 311 781 311c-86 0-153 37-201 111v2h-1l1-2v-95H358c1.333 21.333 2 87.666 2 199 0 111.333-.667 267.666-2 469z"
-        fill="#ffffff"
-        className="color000000 svgShape"
-      ></path>
-    </svg>
-  ),
-};
+
 export function ThreeDCardDemo() {
   return (
     <div id="uniqueUpload" className="h-[60rem] flex-col w-full dark:bg-black bg-[#0A0A0A] dark:bg-grid-white/[0.2] bg-grid-black/[0.2] bg-[length:32px_32px] relative flex items-center justify-center">
