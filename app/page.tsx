924eee53b61a442cd377365bc4ed6a6b957384ff
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-02 18:33:24 +1000

    deleted the icons

--- app/page.tsx
+++ app/page.tsx
@@ -63,13 +63,11 @@ export function AnimatedModalDemo() {
                 </span>
               </div>
               <div className="flex items-center justify-center">
-                <ElevatorIcon className="mr-1 text-neutral-700 dark:text-neutral-300 h-4 w-4" />
                 <span className="text-neutral-700 dark:text-neutral-300 text-sm">
                   12 hotels
                 </span>
               </div>
               <div className="flex items-center justify-center">
-                <VacationIcon className="mr-1 text-neutral-700 dark:text-neutral-300 h-4 w-4" />
                 <span className="text-neutral-700 dark:text-neutral-300 text-sm">
                   69 visiting spots
                 </span>
@@ -109,27 +107,7 @@ export function AnimatedModalDemo() {
 }
  
  
-const ElevatorIcon = ({ className }: { className?: string }) => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={className}
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M5 4m0 1a1 1 0 0 1 1 -1h12a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-12a1 1 0 0 1 -1 -1z" />
-      <path d="M10 10l2 -2l2 2" />
-      <path d="M10 14l2 2l2 -2" />
-    </svg>
-  );
-};
+
  
 const FoodIcon = ({ className }: { className?: string }) => {
   return (
