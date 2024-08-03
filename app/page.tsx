b5ace54c7ee05918fed8f35fc630e8565ed2063d
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-03 18:59:41 +1000

    deleted some whitespace

--- app/page.tsx
+++ app/page.tsx
@@ -22,9 +22,6 @@ import {
   ModalFooter,
   ModalTrigger,
 } from "./components/ui/animated-modal";
-
-
-
 export function AnimatedModalDemo() {
   const images = [
     "https://images.unsplash.com/photo-1517322048670-4fba75cbbb62?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
