866e0e5801a631b71558c35b0f8795867e765768
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-03 20:30:21 +1000

    closed the div tag

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -2,6 +2,7 @@ import { UserProfile } from '@clerk/nextjs';
 
 const ProfilePage = () => {
   return (
+    <div className="h-[50rem] flex-col w-full dark:bg-black bg-[#0A0A0A] dark:bg-grid-white/[0.2] bg-grid-black/[0.2] bg-[length:32px_32px] relative flex items-center justify-center">
     <div className="min-h-screen flex items-center justify-center bg-black">
       <div className="w-full max-w-lg">
         <UserProfile
