d01e0cae0b78359558b0788da5cf023fff0f2106
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-03 19:01:54 +1000

    importr the animated tooltip

--- app/components/AnimatedTooltip.tsx
+++ app/components/AnimatedTooltip.tsx
@@ -1,3 +1,5 @@
+import { AnimatedTooltip } from "./ui/animated-tooltip";
+
 export function AnimatedTooltipPreview() {
     return (
       <div className="tooltipContainer flex flex-row items-center justify-center mb-10 w-full">
--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import styles from "./styles/Home.module.css";
 import { FlipWords } from "./components/ui/flip-words";
 import { cn } from "@/util/cn";
 import { BackgroundGradientAnimation } from "./components/ui/background-gradient-animation";
-import { AnimatedTooltip } from "./components/ui/animated-tooltip";
 import { CardBody, CardContainer, CardItem } from "./components/ui/3d-card";
 import BlurIn from "./components/magicui/blur-in";
 import WordPullUp from "./components/magicui/word-pullup";
