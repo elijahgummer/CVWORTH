d1df349da9a9b483146fd03f7beb1f94362a9945
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-05 16:05:45 +1000

    added the missing dependicies for curY and curX

--- app/components/ui/background-gradient-animation.tsx
+++ app/components/ui/background-gradient-animation.tsx
@@ -82,7 +82,7 @@ export const BackgroundGradientAnimation = ({
     }
 
     move();
-  }, [tgX, tgY]);
+  }, [tgX, tgY, curX, curY]);
 
   const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
     if (interactiveRef.current) {
