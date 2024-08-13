29f438eae6c52cf78a115bb6a89fe22cb7300eff
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-13 19:24:22 +1000

    changed some features

--- app/upload/[[...upload]]/page.jsx
+++ app/upload/[[...upload]]/page.jsx
@@ -1,5 +1,5 @@
 import { cn } from "@/util/cn";
-import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
+import { CardBody, CardContainer, CardItem } from "./components/ui/3d-card";
 import ResumeAnalyzerApp from "./ResumeAnalyzerApp";
 import AnimatedGradientText from "./magicui/animated-gradient-text";
 
