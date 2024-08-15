1d64b6379b55f1ed3838783d6e833633b6760fb9
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-15 21:39:21 +1000

    deleted some whitespace

--- app/pdfjs-dist-shims.d.ts
+++ app/pdfjs-dist-shims.d.ts
@@ -1,11 +1,8 @@
 declare module 'pdfjs-dist/build/pdf.min.mjs' {
   export * from 'pdfjs-dist'
 }
-
 declare module 'pdfjs-dist/build/pdf.mjs' {
   export * from 'pdfjs-dist'
 }
-
 declare module 'pdfjs-dist/build/pdf.worker.min.mjs' {}
-
 declare module 'pdfjs-dist/build/pdf.worker.mjs' {}
