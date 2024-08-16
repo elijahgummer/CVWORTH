5947038c0cef1e0f210d6ebfff8005fb337f0275
Elijah Gummer <elijahgummer5@gmail.com>
2024-08-16 19:52:40 +1000

    deleted some whitespace

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
-
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
